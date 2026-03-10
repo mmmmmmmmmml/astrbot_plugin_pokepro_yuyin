536e30d300115770850f7588ae7fa5e10d470a93
mmmmmmmmmml <148480829+mmmmmmmmmml@users.noreply.github.com>
2026-03-11 07:11:29 +0800

    更新 on_poke.py
    第四部

--- core/on_poke.py
+++ core/on_poke.py
@@ -2,7 +2,7 @@ import copy
 import random
 
 from astrbot.api import logger
-from astrbot.api.message_components import Face
+from astrbot.api.message_components import Face，Record
 from astrbot.core.message.components import At, Plain
 from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import (
     AiocqhttpMessageEvent,
@@ -30,6 +30,7 @@ class GetPokeHandler:
             PokeModel.LLM: self.respond_llm,
             PokeModel.FACE: self.respond_face,
             PokeModel.meme: self.respond_meme,
+            PokeModel.YUYIN: self.respond_yuyin,
             PokeModel.BAN: self.respond_ban,
             PokeModel.COMMAND: self.respond_cmd,
         }
@@ -120,6 +121,17 @@ class GetPokeHandler:
         """回复表情包"""
         img = self.cfg.get_image()
         yield event.image_result(img)
+
+    async def respond_yuyin(self, event: AiocqhttpMessageEvent):
+        """回复语音"""
+        audio_path = self.cfg.get_audio()
+        if audio_path:
+            yield event.chain_result([
+                Record(file=audio_path, url=audio_path)
+            ])
+        else:
+            logger.warning("[戳一戳] 语音池为空，无法发送语音")
+            yield None
 
     async def respond_ban(self, event: AiocqhttpMessageEvent):
         """禁言"""
